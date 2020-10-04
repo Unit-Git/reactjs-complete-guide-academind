d7f5046422ed40c42c674f58e72806e4c3b4f8df
Unit-Git <theunit954@gmail.com>
2020-10-04 14:05:35 +0700

    end #5  Styling React Components & Elements

--- src/App.js
+++ src/App.js
@@ -73,9 +73,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <button className="button" onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
         <br />
         <br />
+
         {persons}
       </div>
     );
