04205c55c4557b4f1abc4b8de1e476eb53b4bf69
Unit-Git <theunit954@gmail.com>
2020-10-06 13:42:45 +0700

    Settinh State Correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,10 +25,6 @@ class App extends Component {
     return state;
   }
 
-  // componentWillMount() {
-  //   console.log("[App.js] componentWillMount");
-  // }
-
   componentDidMount() {
     console.log("[App.js] componentDidMount");
   }
@@ -55,8 +51,11 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons,
+    this.setState((prevState, props) => {
+      return {
+        ...prevState,
+        persons: persons,
+      };
     });
   };
 
